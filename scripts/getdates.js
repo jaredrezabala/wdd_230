390ad34da53f9ce6e2150cccf1f3d2394f439aeb
jaredrezabala <rezabalajared@gmail.com>
2024-01-17 20:17:24 -0500

    erros on course webpage based on feedback

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,6 +1,6 @@
 var year = new Date().getFullYear();
-var currentDate = new Date();
 var p1 = document.getElementById("copyright");
 p1.textContent = "© " + year + " Jared Rezabala - Ecuador"
-var p2 = document.getElementById("lastModified");
-p2.textContent = "Last Modification: " + currentDate;
+var lastModified = document.getElementById("lastModified");
+var lastModifiedDate = document.lastModified;
+lastModified.innerHTML = "Last Modified: " + lastModifiedDate;
