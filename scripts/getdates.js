8245673590d4163209d7fbf28c616826787e8d43
jaredrezabala <rezabalajared@gmail.com>
2024-01-18 20:51:38 -0500

    getdate.js updated

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,6 +1,6 @@
 var year = new Date().getFullYear();
 var p1 = document.getElementById("copyright");
 p1.textContent = "© " + year + " Jared Rezabala - Ecuador"
-var lastModified = document.getElementById("lastModified");
-var lastModifiedDate = document.lastModified;
-lastModified.innerHTML = "Last Modified: " + lastModifiedDate;
+let lastModified = document.lastModified;
+let lastDate = document.getElementById("lastModified");
+lastDate.innerHTML = "Last Modified on: " + lastModified;
