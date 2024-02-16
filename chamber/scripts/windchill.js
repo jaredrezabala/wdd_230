ad049fc2f5e4b9ac46a9d8929224503be7c15086
jaredrezabala <rezabalajared@gmail.com>
2024-02-16 18:54:29 -0500

    chamber enhanced page

---
+++ chamber/scripts/windchill.js
@@ -0,0 +1,10 @@
+// const temp = document.getElementById('temperature');
+// const windSpeed = document.getElementById('wind-speed');
+// const windChill = document.getElementById('wind-chill');
+
+// let temperature = parseFloat(temp.innerText);
+// let wind = parseFloat(windSpeed.innerText);
+
+// if(temperature <= 50 && wind > 3.0 ){
+//     var chill = calcu
+// }
