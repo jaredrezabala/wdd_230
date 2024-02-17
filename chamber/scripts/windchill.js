9185d29a795756d457f43bbe220da5b2195ed712
jaredrezabala <rezabalajared@gmail.com>
2024-02-16 19:35:58 -0500

    last changes

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,10 +1,7 @@
-// const temp = document.getElementById('temperature');
-// const windSpeed = document.getElementById('wind-speed');
-// const windChill = document.getElementById('wind-chill');
+const temp = document.getElementById('#temperature');
+const windSpeed = document.getElementById('#wind-speed');
+const windChill = document.getElementById('#wind-chill');
 
-// let temperature = parseFloat(temp.innerText);
-// let wind = parseFloat(windSpeed.innerText);
-
-// if(temperature <= 50 && wind > 3.0 ){
-//     var chill = calcu
-// }
+let wc = 35.74 + 0.6215 * temp - 35.75 * Math.pow(windSpeed, 0.16) + 0.4275 * temp * Math.pow(windSpeed, 0.16);
+console.log(wc);
+windChill = wc;
