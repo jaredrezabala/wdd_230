9dacfd0392e7db0e0258ee0142aea94ad99f40ed
jaredrezabala <rezabalajared@gmail.com>
2024-03-04 19:02:21 -0500

    windchill updated

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,7 +1,17 @@
-const temp = document.getElementById('#temperature');
-const windSpeed = document.getElementById('#wind-speed');
-const windChill = document.getElementById('#wind-chill');
 
-let wc = 35.74 + 0.6215 * temp - 35.75 * Math.pow(windSpeed, 0.16) + 0.4275 * temp * Math.pow(windSpeed, 0.16);
-console.log(wc);
-windChill = wc;
+var temp = document.getElementById('temperature');
+var windSpeed = document.getElementById('wind-speed');
+var windChill = document.getElementById('windchill');
+
+function calculateWindChill(temperature, speed) {
+    if (temperature <= 50 && speed > 3.0) {
+        var chill = 35.74 + 0.6215 * temperature - 35.75 * Math.pow(speed, 0.16) + 0.4275 * temperature * Math.pow(speed, 0.16);
+        chill = Math.round(chill * 100) / 100;
+        windChill.textContent = chill;
+    } else {
+        document.getElementById('windchill').textContent = "Loading..."
+    }
+}
+
+calculateWindChill(temp.textContent, windSpeed.textContent);
+
