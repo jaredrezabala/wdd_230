f728696a4fefdc7760c384e42ba13b00992857b2
jaredrezabala <rezabalajared@gmail.com>
2024-03-14 18:05:19 -0500

    chamber website finished

--- chamber/scripts/weather.js
+++ chamber/scripts/weather.js
@@ -10,7 +10,6 @@ async function getWeather() {
     const response = await fetch(weatherUrl);
     const data = await response.json();
     displayWeather(data);
-    displayForecast(data);
 }
 
 function displayWeather(data) {
@@ -24,4 +23,46 @@ function displayWeather(data) {
 
 }
 
+async function getForecast() {
+    const forecastResponse = await fetch(forecast);
+    const forecastData = await forecastResponse.json();
+    displayForecast(forecastData);
+}
+
+function displayForecast(forecastData) {
+    const threeDayForecast = forecastData.list.filter(item => item.dt_txt.includes('15:00:00'));
+    let day = 0;
+    const weekDays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+    threeDayForecast.forEach(forecastDay => {
+        const d = new Date(forecastDay.dt_txt);
+        const dayOfWeek = weekDays[d.getDay()];
+        const forecastTemp = forecastDay.main.temp;
+        const iconsrc = `https://openweathermap.org/img/w/${forecastDay.weather[0].icon}.png`;
+        const icon = document.createElement('img');
+        icon.setAttribute('src', iconsrc);
+        const forecastItem = document.createElement('p');
+        forecastItem.textContent = `${dayOfWeek}: ${Math.trunc(forecastTemp)} °F`;
+        forecastItem.appendChild(icon);
+        tempForecast.appendChild(forecastItem);
+        day++;
+    });
+}
+getForecast();
 getWeather();
+
+
+// BANNER 
+document.addEventListener("DOMContentLoaded", function () {
+    var today = new Date();
+    var dayOfWeek = today.getDay(); // 0 for Sunday, 1 for Monday, 2 for Tuesday, etc.
+
+    // Check if it's Monday, Tuesday, or Wednesday
+    if (dayOfWeek >= 1 && dayOfWeek <= 3) {
+        document.getElementById("chamberBanner").style.display = "block";
+    }
+
+    // Close banner functionality
+    document.getElementById("closeBannerBtn").addEventListener("click", function () {
+        document.getElementById("chamberBanner").style.display = "none";
+    });
+});
